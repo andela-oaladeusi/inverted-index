f06719e21c047e96960b049d4a1fa2290836f607
andela-oaladeusi <olawale.aladeusi@andela.com>
2016-11-22 19:07:15 +0100

    Add class contructor to initialize dependent variable to InvertedIndexClass

--- src/js/inverted-index.js
+++ src/js/inverted-index.js
@@ -6,11 +6,16 @@
  * 
  */
 
-
 //  Main Class
 class InvertedIndexClass {
-    
-
+    // constructor use to initialize identifier
+    constructor() {
+        this.allIndexMap = []; //This variable array will keep track of all index object
+        this.json = []; // json array will hold our json files for multiple upload
+        this.fileName = null; //fileName: Name of each json file`
+        this.indexName = {}; // it will hold all json index for different json files
+        this.len1 = {}; //len1 : it will keep the length of each json file map up with there names
+    }
 // method to validate json content
     validateJsonContent(jUpload) {
         if (jUpload.hasOwnProperty('title') && jUpload.hasOwnProperty('text')) {

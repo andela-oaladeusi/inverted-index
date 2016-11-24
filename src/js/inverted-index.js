1e202b1866c8abf0ad764d5000966061fcc31d3c
andela-oaladeusi <olawale.aladeusi@andela.com>
2016-11-24 01:49:13 +0100

    Remove redundancy code

--- src/js/inverted-index.js
+++ src/js/inverted-index.js
@@ -8,33 +8,41 @@
 
 //  Main Class
 class InvertedIndexClass {
-    // constructor use to initialize identifier
+
+    /** constructor use to initialize identifier
+     * 'json' array holds our json files
+     * fileName: Name of each json file`
+     * it will hold all json index for different json files
+     * jLength : it will keep the length of each json file map up with there names
+    */
     constructor() {
-        this.allIndexMap = []; //This variable array will keep track of all index object
-        this.json = []; // json array will hold our json files for multiple upload
-        this.fileName = null; //fileName: Name of each json file`
-        this.indexName = {}; // it will hold all json index for different json files
-        this.len1 = {}; //len1 : it will keep the length of each json file map up with there names
+        this.json = null;
+        this.fileName = null; 
+        this.indexName = {};  
+        this.jLength = {}; 
     }
-    // Create Index method, the method is use to generate the index for a json file
-    // It accept two parameters, Json file and the name of the Json file
+
+    /**
+     * Create Index method, the method is use to generate the index for a json file
+     * It accept two parameters, Json file and the name of the Json file
+    */
     createIndex(jsonArray, jsonName) {
-        let file = jsonArray[0];
         let titleTextArray = [];
         let singleJsonIndex = {};
-        this.len1[jsonName] = jsonArray[0].length;
+        this.jLength[jsonName] = jsonArray.length;
 
-        // This accept Json file, and pick all the document in it seperately,
-        //  merging text and title key of the document for each iteration
-        for (let index in file) {
-            let documentArray = file[index];
+        /** This accept Json file, and pick all the document in it seperately
+         * merging text and title key of the document for each iteration
+         */
+        for (let index in jsonArray) {
+            let documentArray = jsonArray[index];
             let rawTitleArr = documentArray.title.toLowerCase().match(/\w+/g);
             let rawTextArr = documentArray.text.toLowerCase().match(/\w+/g);
             let joinTitleText = [...new Set([...rawTitleArr, ...rawTextArr])];
             titleTextArray.push(joinTitleText);
         }
 
-       // This loop arrange the index
+        // This loop arrange the index
         for (let index in titleTextArray) {
             titleTextArray[index].forEach(function (key) {
                 if (singleJsonIndex[key]) {
@@ -50,6 +58,7 @@ class InvertedIndexClass {
             });
         }
         this.indexName[jsonName] = singleJsonIndex;
+        return singleJsonIndex;
     }
 
     //  Method to get JSON's index' and it takes one parameter, name of selected JSON
@@ -57,81 +66,38 @@ class InvertedIndexClass {
         return this.indexName[jsonName];
     }
 
-    //  Search method, it takes two parameter, term to search and filter's' name
-  /*  searchIndex(term, filterName) {
+    // Method to search all files
+    searchIndex(term, filterName) {
         let searchResult = {};
         let allSearchTerm = term.toLowerCase().match(/\w+/g);
-        let searchSingleJson = this.indexName[filterName];
-        allSearchTerm.forEach(function (eachWord) {
-            if (eachWord in searchSingleJson) {
-                searchResult[eachWord] = searchSingleJson[eachWord];
-            }
-        });
-        return searchResult;
-    } */
-
-// Method to search all files
-   searchIndex(term, filterName) {
-       let searchResult={};
-       let allSearchTerm = term.toLowerCase().match(/\w+/g);
-       if(filterName==='all'){
-           for(let key in this.indexName ){
-               console.log(key);
-               let searchResultKey={};
-               let searchSingleJson = this.indexName[key];
+        if (filterName === 'all') {
+            for (let key in this.indexName) {
+                console.log(key);
+                let searchResultKey = {};
+                let searchSingleJson = this.indexName[key];
                 allSearchTerm.forEach(function (eachWord) {
                     if (eachWord in searchSingleJson) {
                         searchResultKey[eachWord] = searchSingleJson[eachWord];
-                     }
+                    }
                 });
-            searchResult[key]=searchResultKey;
+                searchResult[key] = searchResultKey;
             }
             return searchResult;
-            // console.log(searchResult);
-       }
-       else{
+        }
+        else {
             let searchSingleJson = this.indexName[filterName];
             allSearchTerm.forEach(function (eachWord) {
                 if (eachWord in searchSingleJson) {
                     searchResult[eachWord] = searchSingleJson[eachWord];
                 }
             });
-        return searchResult;
-        // console.log(searchResult);
-       }
-    }
-
-
-    // method to read file uploaded and validate it 
-    getFile(filePath) {
-        let _this = this;
-        let regex = /^([a-zA-Z0-9\s_\\.\-:])+(.json)$/;
-        if (regex.test($("#fileUpload").val().toLowerCase())) {
-            if (typeof (FileReader) != "undefined") {
-                let reader = new FileReader();
-                this.fileName = filePath.name;
-                reader.onload = function (e) {
-                    let jUpload = JSON.parse(reader.result);
-                    if (_this.isValidJson(jUpload) && (!_this.isJsonEmpty(jUpload))) {
-                        _this.json.push(jUpload);
-                    }
-                    else {
-                        alert("Invalid Json File");
-                        return;
-                    }
-                };
-                reader.readAsText(filePath);
-            } else {
-                alert("This browser does not support HTML5.");
-            }
-        } else {
-            alert("Please upload a valid JSON file.");
+            return searchResult;
         }
     }
 
     // method to validate json content
     validateJsonContent(jUpload) {
-        if (jUpload.hasOwnProperty('title') && jUpload.hasOwnProperty('text')) {
+        if (jUpload[0].title && jUpload[0].text) {
             return true;
         }
         else {
@@ -141,22 +107,21 @@ class InvertedIndexClass {
     }
 
     // Methdo to check if Json is valid or not
-    isValidJson(item) {
-        item = typeof item !== "string" ? JSON.stringify(item) : item;
+    isValidJson(jUpload) {
+        if (jUpload && typeof jUpload === "object") {
+            return true;
+        }
         try {
-            item = JSON.parse(item);
+            jUpload = JSON.parse(jUpload);
         } catch (e) {
             return false;
         }
-        if (typeof item === "object" && item !== null) {
-            return true;
-        }
-        return false;
+        return true;
     }
 
     // Method to check if json is empty or not
-    isJsonEmpty(item) {
-        if (item.length === 0) {
+    isJsonEmpty(jUpload) {
+        if (jUpload.length === 0) {
             return true;
         }
         else {

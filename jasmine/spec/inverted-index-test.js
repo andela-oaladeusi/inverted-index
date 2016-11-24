4cad43f0adb6d30ea3ceffa9c34bf8029c2957d6
andela-oaladeusi <olawale.aladeusi@andela.com>
2016-11-24 01:46:59 +0100

    Add test cases for populate and search index

--- jasmine/spec/inverted-index-test.js
+++ jasmine/spec/inverted-index-test.js
@@ -1,76 +1,118 @@
 'use strict';
-let invertedIndexObject = new InvertedIndexClass();
+let invertedIndexTest = new InvertedIndexClass();
 
-
-describe("All Test Suites", function () {
-  // initialize contants to be use to test
+describe("Inverted Index Test Suites", function () {
+  // Initialize constants to be use for testing
   const books = [
     {
       "title": "Alice in Wonderland",
-      "text": "Alice falls into a rabbit hole and enters a world full of imagination."
+      "text": "Alice falls into a rabbit hole and enters a world full of"
     },
 
     {
       "title": "The Lord of the Rings: The Fellowship of the Ring.",
-      "text": "An unusual alliance of man, elf, dwarf, wizard and hobbit seek to destroy a powerful ring."
+      "text": "An unusual alliance of man, elf, dwarf, wizard and hobbit."
     }
   ];
+
+  const books2 = [
+    {
+      "title": "Alice in Wonderland",
+      "text": "Alice falls into a rabbit hole and enters a world full of ."
+    },
+
+    {
+      "title": "The Lord of the Rings: The Fellowship of the Ring.",
+      "text": "An unusual alliance of man, elf, dwarf, wizard and hobbit."
+    },
+    {
+      "title": "Alice in Wonderland",
+      "text": "Alice falls into a rabbit hole and enters a world full of ."
+    },
+
+    {
+      "title": "The Lord of the Rings: The Fellowship of the Ring.",
+      "text": "An unusual alliance of man, elf, dwarf, wizard and hobbit."
+    }
+  ];
+
   const emptyBook = [];
-
-
   const wrongFomatBook = [
     {
       "noooooo": "Alice in Wonderland",
-      "texteee": "Alice falls into a rabbit hole and enters a world full of imagination."
+      "texteee": "Alice falls into a rabbit hole and enters a world ."
     }
   ];
-  const bookIndex={ alice: { 0: true },
-  in: { 0: true },
-  of: { 0: true ,1: true },
-  the: { 0: true, 1: true },
-  wonderland: { 0: true },
-  falls: { 0: true },
-  into: { 0: true },
-  a: { 0: true },
-  pit: { 0: true },
-  and: { 0: true, 1: true },
-  dies: { 0: true, 1: true },
-  lord: { 1: true },
-  rings: { 1: true },
-  an: { 1: true },
-  unusual: { 1: true },
-  alliance: { 1: true },
-  man: { 1: true },
-  who: { 1: true },
-  live: { 1: true } };
 
   const invalidJson = " 'wale','deji','ola', 'andela','class' ";
 
+  let readindex = invertedIndexTest.createIndex(books, "booksjson");
+  let jsonindex = invertedIndexTest.getIndex('booksjson');
+  let readindex2 = invertedIndexTest.createIndex(books2, "booksjson2");
+  let jsonindex2 = invertedIndexTest.getIndex('booksjson2');
+
   describe("Read book data", function () {
 
-    it("Should be a valid JSON Array", function () {
-      expect(invertedIndexObject.isValidJson(books)).toBeTruthy();
+    it("Should return be a valid JSON Array", function () {
+      expect(invertedIndexTest.isValidJson(books)).toBeTruthy();
     });
 
+    it("Should return an invalid JSON Array when parameter is 'invalidJson'",
+     function () {
+      expect(invertedIndexTest.isValidJson(invalidJson)).toBeFalsy();
+    });
 
-    it("Should return an invalid JSON Array", function () {
-      expect(invertedIndexObject.isValidJson(invalidJson)).toBeFalsy();
+    it("Should return true, when our parameter is 'emptyBook'", function () {
+      expect(invertedIndexTest.isJsonEmpty(emptyBook)).toBeTruthy();
     });
 
     it("Should return true JSON array is not empty", function () {
-      expect(invertedIndexObject.isJsonEmpty(emptyBook)).toBeTruthy();
+      expect(invertedIndexTest.isJsonEmpty(books)).toBeFalsy();
     });
 
-    it("Should return true JSON array is not empty", function () {
-      expect(invertedIndexObject.isJsonEmpty(books)).toBeFalsy();
+    it("Should return false, when json contents is not properly formatted", 
+    function () {expect(invertedIndexTest.validateJsonContent(wrongFomatBook))
+      .toBeFalsy();
+    });
+  });
+
+  describe("Populate Index", function () {
+    it("Index should be created after reading json file", function () {
+      expect(typeof readindex).toEqual('object');
     });
 
-    it("Should return false, when json contents is not properly formatted", function () {
-      expect(invertedIndexObject.validateJsonContent(wrongFomatBook)).toBeFalsy();
+    it("Index should be created after reading json file for the second json",
+     function () {
+      expect(typeof readindex2).toEqual('object');
     });
 
-    it("Should return True, when json contents is properly formatted", function () {
-      expect(invertedIndexObject.validateJsonContent(books)).toEqual(bookIndex);
+    it("It should return correct index", function () {
+      expect(jsonindex.alice).toEqual({ 0: true });
+    });
+
+    it("It should return correct index when passing 'and'", function () {
+      expect(jsonindex.and).toEqual({ 0: true, 1: true });
+    });
+
+    it("Ensures index is correct using another json file", function () {
+      expect(jsonindex2.alice).toEqual({ 0: true, 2: true });
+    });
+
+  });
+
+describe("Search Index", function () {
+    it("It should return the correct search index for just one single file",
+     function () {
+      expect(invertedIndexTest.searchIndex('alice and', 'booksjson')).toEqual(
+        { alice: { 0: true }, and: { 0: true, 1: true } });
+    });
+
+    it("It should return the correct search index for all files", function () {
+      expect(invertedIndexTest.searchIndex('alice and', 'all')).toEqual(
+        {booksjson: {alice: {0: true}, 
+      and: {0: true, 1: true}},booksjson2: {alice: {0: true, 2: true}, 
+      and: {0: true, 1: true, 2: true, 3: true}}
+      });
     });
 
   });

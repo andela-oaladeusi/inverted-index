a76fb38937d824c479de9848e5c2806596d185b7
andela-oaladeusi <olawale.aladeusi@andela.com>
2016-12-05 18:49:11 +0100

    Indent all files with 2 spaces

--- src/js/inverted-index.js
+++ src/js/inverted-index.js
@@ -4,175 +4,173 @@
  * @class
  */
 class InvertedIndexClass {
-	/**
-	 * class constructor
-	 * @constructor
-	 */
-	constructor() {
-		this.json = null;
-		this.fileName = null;
-		this.allFileIndex = {};
-		this.jLength = {};
-		this.names = [];
-		this.singleJsonIndex = {};
-	}
+  /**
+   * class constructor
+   * @constructor
+  **/
+  constructor() {
+    this.json = null;
+    this.fileName = null;
+    this.allFileIndex = {};
+    this.jLength = {};
+    this.names = [];
+    this.singleJsonIndex = {};
+  }
 
-	/**
-	 * Create index
-	 * @param {Array} jsonArray
-	 * @param {string} jsonName
-	 * @return {Object} singleIndex
-	 */
-	createIndex(jsonArray, jsonName) {
+  /**
+   * Create index
+   * @param {Array} jsonArray
+   * @param {string} jsonName
+   * @return {Object} singleIndex
+   */
+  createIndex(jsonArray, jsonName) {
 
-		if (!this.validateJsonContent(jsonArray)) {
-			document.getElementById('alert-message').innerHTML = "Invalid Json Content";
-			$('#alertError').modal('show');
-			return;
-		}
+    if (!this.validateJsonContent(jsonArray)) {
+      document.getElementById('alert-message').innerHTML = "Invalid Json Content";
+      $('#alertError').modal('show');
+      return;
+    }
 
-		let titleTextArray = [];
+    let titleTextArray = [];
 
-		this.jLength[jsonName] = jsonArray.length;
+    this.jLength[jsonName] = jsonArray.length;
 
 		// This iteration will merge both title and text property of each files
-		for (let countFile in jsonArray) {
-			let documentArray = jsonArray[countFile];
-			let rawTitleArr = documentArray.title.toLowerCase().match(/\w+/g);
-			let rawTextArr = documentArray.text.toLowerCase().match(/\w+/g);
-			let joinTitleText = [...new Set([...rawTitleArr, ...rawTextArr])];
-			titleTextArray.push(joinTitleText);
-		}
+    for (let countFile in jsonArray) {
+      let documentArray = jsonArray[countFile];
+      let rawTitleArr = documentArray.title.toLowerCase().match(/\w+/g);
+      let rawTextArr = documentArray.text.toLowerCase().match(/\w+/g);
+      let joinTitleText = [...new Set([...rawTitleArr, ...rawTextArr])];
+      titleTextArray.push(joinTitleText);
+    }
 
 		// This iteration will arrange the index 
-		for (let countFile in titleTextArray) {
-			this.arrangeIndex(countFile, titleTextArray);
-		}
+    for (let countFile in titleTextArray) {
+      this.arrangeIndex(countFile, titleTextArray);
+    }
 
 		// sorted index
-		let sortedIndex = {};
-		Object.keys(this.singleJsonIndex)
-			.sort()
-			.forEach((key) => {
-				sortedIndex[key] = this.singleJsonIndex[key];
-			});
+    let sortedIndex = {};
+    Object.keys(this.singleJsonIndex)
+      .sort()
+      .forEach((key) => {
+	      sortedIndex[key] = this.singleJsonIndex[key];
+	    });
 
+    this.allFileIndex[jsonName] = sortedIndex;
+    this.singleJsonIndex = {};
 
-		this.allFileIndex[jsonName] = sortedIndex;
-		this.singleJsonIndex = {};
+    return sortedIndex;
+  }
 
-		return sortedIndex;
-	}
+  /**
+   * arrange index
+   * @param {Integer} countFile
+   * @param {Array} titleTextArray
+  */
+  arrangeIndex(countFile, titleTextArray) {
+    let _this = this;
 
-	/**
-	 * arrange index
-	 * @param {Integer} countFile
-	 * @param {Array} titleTextArray
-	 */
-	arrangeIndex(countFile, titleTextArray) {
-		let _this = this;
-
-		titleTextArray[countFile].forEach((key) => {
-			if (_this.singleJsonIndex[key]) {
-				if (!_this.singleJsonIndex[key][countFile]) {
-					_this.singleJsonIndex[key][countFile] = true;
-				}
-			} else {
-				let oneIndex = {};
-				oneIndex[countFile] = true;
-				_this.singleJsonIndex[key] = oneIndex;
-			}
-		});
-
-	}
+    titleTextArray[countFile].forEach((key) => {
+    if (_this.singleJsonIndex[key]) {
+      if (!_this.singleJsonIndex[key][countFile]) {
+        _this.singleJsonIndex[key][countFile] = true;
+       }
+      } else {
+        let oneIndex = {};
+        oneIndex[countFile] = true;
+        _this.singleJsonIndex[key] = oneIndex;
+      }
+    });
+  }
 
 	/**
  * Get a particular index
  * @param {String} jsonName
  * @return {Object}
  */
-	getIndex(jsonName) {
-		return this.allFileIndex[jsonName];
-	}
+  getIndex(jsonName) {
+    return this.allFileIndex[jsonName];
+  }
 
-	/**
- * Search Index.
- * @param {String} query query string
- * @param {String} filterName name of index to be searched.
- * @return {Object} searchResult 
- */
-	searchIndex(query, filterName) {
-		let searchResult = {};
-		const allSearchQuery = query.toLowerCase().match(/\w+/g);
+  /**
+   * Search Index.
+   * @param {String} query query string
+   * @param {String} filterName name of index to be searched.
+   * @return {Object} searchResult 
+  */
+  searchIndex(query, filterName) {
+    let searchResult = {};
+    const allSearchQuery = query.toLowerCase().match(/\w+/g);
 
-		if (filterName === 'all') {
-			for (let key in this.allFileIndex) {
-				let searchResultKey = {};
-				let searchSingleJson = this.allFileIndex[key];
+    if (filterName === 'all') {
+      for (let key in this.allFileIndex) {
+        let searchResultKey = {};
+        let searchSingleJson = this.allFileIndex[key];
 
-				allSearchQuery.forEach((eachQuery) => {
-					if (eachQuery in searchSingleJson) {
-						searchResultKey[eachQuery] = searchSingleJson[eachQuery];
-					} else {
-						searchResultKey[eachQuery] = { 0: false };
-					}
-				});
-				searchResult[key] = searchResultKey;
-			}
-			return searchResult;
-		}
-		else {
-			let searchSingleJson = this.allFileIndex[filterName];
-			allSearchQuery.forEach((eachQuery) => {
-				if (eachQuery in searchSingleJson) {
-					searchResult[eachQuery] = searchSingleJson[eachQuery];
-				} else {
-					searchResult[eachQuery] = { 0: false };
-				}
+        allSearchQuery.forEach((eachQuery) => {
+          if (eachQuery in searchSingleJson) {
+            searchResultKey[eachQuery] = searchSingleJson[eachQuery];
+          } else {
+            searchResultKey[eachQuery] = { 0: false };
+          }
+        });
+        searchResult[key] = searchResultKey;
+      }
+      return searchResult;
+    }
+    else {
+      let searchSingleJson = this.allFileIndex[filterName];
+      allSearchQuery.forEach((eachQuery) => {
+        if (eachQuery in searchSingleJson) {
+          searchResult[eachQuery] = searchSingleJson[eachQuery];
+        } else {
+          searchResult[eachQuery] = { 0: false };
+        }
 
-			});
-			return searchResult;
-		}
-	}
+      });
+      return searchResult;
+    }
+  }
 
-	/**
-* validateJsonContent.
-* @param {Object} jUpload
-* @return {Boolean} true or false 
-*/
-	validateJsonContent(jUpload) {
-		try {
-			return jUpload[0].title && jUpload[0].text ? true : false;
-		} catch (e) {
-			return false;
-		}
-	}
+  /**
+   *validateJsonContent.
+   *@param {Object} jUpload
+   *@return {Boolean} true or false 
+  */
+  validateJsonContent(jUpload) {
+    try {
+      return jUpload[0].title && jUpload[0].text ? true : false;
+    } catch (e) {
+      return false;
+    }
+  }
 
-	/**
-* isValidJson
-* @param {Object} jUpload
-* @return {Boolean} true or false 
-*/
-	isValidJson(jUpload) {
-		if (jUpload && typeof jUpload === "object") {
-			return true;
-		}
-		try {
-			jUpload = JSON.parse(jUpload);
-			return true;
-		} catch (e) {
-			return false;
-		}
-	}
+  /**
+   *isValidJson
+   *@param {Object} jUpload
+   *@return {Boolean} true or false 
+  */
+  isValidJson(jUpload) {
+    if (jUpload && typeof jUpload === "object") {
+      return true;
+    }
+    try {
+      jUpload = JSON.parse(jUpload);
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
 
-	/**
-* isJsonEmpty
-* @param {Object} jUpload
-* @return {Boolean} true or false 
-*/
-	isJsonEmpty(jUpload) {
-		return jUpload.length === 0 ? true : false;
-	}
+  /**
+   *isJsonEmpty
+   *@param {Object} jUpload
+   *@return {Boolean} true or false 
+  */
+  isJsonEmpty(jUpload) {
+    return jUpload.length === 0 ? true : false;
+  }
 }
 
 // Create instance for InvertedIndexClass
--- src/js/main.js
+++ src/js/main.js
@@ -4,14 +4,14 @@ let noFile = true;
 
 // create index button click
 $('#create-index').click(() => {
-	createIndexButton();
+  createIndexButton();
 });
 
 // displayAllSearch function
 let displayAllSearch = (dataIndex) => {
-	for (let tableName in dataIndex) {
-		checkIndex(dataIndex[tableName], invertedClass.jLength[tableName], tableName);
-	}
+  for (let tableName in dataIndex) {
+    checkIndex(dataIndex[tableName], invertedClass.jLength[tableName], tableName);
+  }
 };
 
 /**
@@ -19,106 +19,106 @@ let displayAllSearch = (dataIndex) => {
 * This will display all search query in a table
 */
 let checkIndex = (dataIndex, length, fileName) => {
-	let indexDiv = '<br><br><br><div class = "indexDiv"><h2 id = "titleHeader">' +
-		fileName + '</h2>';
+  let indexDiv = '<br><br><br><div class = "indexDiv"><h2 id = "titleHeader">' +
+    fileName + '</h2>';
 
-	indexDiv += '<table class = "responstable">';
+  indexDiv += '<table class = "responstable">';
 
-	for (let headLoop = 0; headLoop <= length; headLoop++) {
-		if (headLoop === 0) {
-			indexDiv += '<thead><tr><th>Terms</th>';
-		}
-		else {
-			indexDiv += '<th>doc_' + headLoop + '</th>';
-		}
-	}
-	indexDiv += '</tr></thead>';
+  for (let headLoop = 0; headLoop <= length; headLoop++) {
+    if (headLoop === 0) {
+      indexDiv += '<thead><tr><th>Terms</th>';
+    }
+    else {
+      indexDiv += '<th>doc_' + headLoop + '</th>';
+    }
+  }
+  indexDiv += '</tr></thead>';
 
-	// This iteration will mark each column in tbody
-	indexDiv += '<tbody>';
-	for (let term in dataIndex) {
-		indexDiv += '<tr><td>' + term + '</td>';
+  // This iteration will mark each column in tbody
+  indexDiv += '<tbody>';
+  for (let term in dataIndex) {
+    indexDiv += '<tr><td>' + term + '</td>';
 
-		for (let column = 0; column < length; column++) {
-			if (dataIndex[term][column]) {
-				indexDiv += '<td> <i class = "fa fa-check"' +
-					'style = "font-size:15px"></i> </td>';
-			} else {
-				indexDiv += '<td> <i class = "fa fa-times-circle-o"' +
-					'aria-hidden = "true"></i></td>';
-			}
-		}
-		indexDiv += '</tr>';
-	}
-	indexDiv += '</tbody></table>';
-	$('.indexContainer').append(indexDiv);
+    for (let column = 0; column < length; column++) {
+      if (dataIndex[term][column]) {
+        indexDiv += '<td> <i class = "fa fa-check"' +
+          'style = "font-size:15px"></i> </td>';
+      } else {
+        indexDiv += '<td> <i class = "fa fa-times-circle-o"' +
+          'aria-hidden = "true"></i></td>';
+      }
+    }
+    indexDiv += '</tr>';
+  }
+  indexDiv += '</tbody></table>';
+  $('.indexContainer').append(indexDiv);
 };
 
 // On keypress , it will call search function
 $('#search-input').keypress(() => {
-	try {
-		searchFunction();
-	} catch (e) {
-		return;
-	}
+  try {
+    searchFunction();
+  } catch (e) {
+    return;
+  }
 });
 
 // On Enter button press, it will call search function
 $('#search-input').keypress((e) => {
-	if (e.which === 13) {
-		try {
-			searchFunction();
-		} catch (e) {
-			return;
-		}
-	}
+  if (e.which === 13) {
+    try {
+      searchFunction();
+    } catch (e) {
+      return;
+    }
+  }
 });
 
 // Json file upload
 document.getElementById('fileUpload').addEventListener('change', (e) => {
-	getFile(e.target.files[0]);
+  getFile(e.target.files[0]);
 });
 
 // Create index button click
 let createIndexButton = () => {
-	if (document.getElementById('fileUpload').value === "") {
-		document.getElementById('alert-message').innerHTML = "Upload a json file";
-		$('#alertError').modal('show');
-		return;
-	}
+  if (document.getElementById('fileUpload').value === "") {
+    document.getElementById('alert-message').innerHTML = "Upload a json file";
+    $('#alertError').modal('show');
+    return;
+  }
 
-	if (!checkExistingFileName()) {
-		let listOfFileName = [];
-		invertedClass.names.push(invertedClass.fileName);
-		invertedClass.createIndex(invertedClass.json, invertedClass.fileName);
+  if (!checkExistingFileName()) {
+    let listOfFileName = [];
+    invertedClass.names.push(invertedClass.fileName);
+    invertedClass.createIndex(invertedClass.json, invertedClass.fileName);
 
-		let fileName = invertedClass.fileName;
-		listOfFileName.push(fileName);
+    let fileName = invertedClass.fileName;
+    listOfFileName.push(fileName);
 
-		// dynamically populate dropdown list with uploaded file names for search
-		$.each(listOfFileName, function () {
-			options.append($("<option />").val(this).text(this));
-		});
+   // dynamically populate dropdown list with uploaded file names for search
+    $.each(listOfFileName, function () {
+      options.append($("<option />").val(this).text(this));
+    });
 
-		// dynamically populate dropdown list with uploaded file names for index
-		$.each(listOfFileName, function () {
-			indexListOptions.append($("<option />").val(this).text(this));
-		});
+    // dynamically populate dropdown list with uploaded file names for index
+    $.each(listOfFileName, function () {
+      indexListOptions.append($("<option />").val(this).text(this));
+    });
 
-		deleteTable();
-		checkIndex(invertedClass.getIndex(fileName),
-			invertedClass.jLength[fileName], fileName, setInputEmpty());
-		invertedClass.json = null;
-		invertedClass.fileName = null;
-		invertedClass.singleJsonIndex = {};
-		noFile = false;
-	}
+    deleteTable();
+    checkIndex(invertedClass.getIndex(fileName),
+      invertedClass.jLength[fileName], fileName, setInputEmpty());
+    invertedClass.json = null;
+    invertedClass.fileName = null;
+    invertedClass.singleJsonIndex = {};
+    noFile = false;
+  }
 
 };
 
 // Set input element of type file to ""
 let setInputEmpty = () => {
-	document.getElementById('fileUpload').value = "";
+  document.getElementById('fileUpload').value = "";
 };
 
 // filter search option
@@ -129,98 +129,98 @@ let indexListOptions = $("#index-drop-list");
 
 // create index table upon selecting any option from select button
 $('#index-drop-list').on('click', function () {
-	let jsonName = $(this).val();
-	return jsonName === null ? console.log('No file to select') : showIndex(jsonName);
+  let jsonName = $(this).val();
+  return jsonName === null ? console.log('No file to select') : showIndex(jsonName);
 });
 
 // get and display index
 let showIndex = (jsonName) => {
-	deleteTable();
-	checkIndex(invertedClass.getIndex(jsonName), invertedClass.jLength[jsonName],
-		jsonName);
+  deleteTable();
+  checkIndex(invertedClass.getIndex(jsonName), invertedClass.jLength[jsonName],
+    jsonName);
 };
 
 // Delete table and create a new table container
 let deleteTable = () => {
-	$("#table-holder").remove();
-	$('figure').append('<div class= "indexContainer" id = "table-holder"></div>');
+  $("#table-holder").remove();
+  $('figure').append('<div class= "indexContainer" id = "table-holder"></div>');
 };
 
 // search function
 let searchFunction = () => {
-	if (noFile) {
-		document.getElementById('alert-message')
-			.innerHTML = "No created index yet\n Create index first";
-		$('#alertError').modal('show');
-		return;
-	}
-	deleteTable();
-	let searchValue = $('#search-input').val();
-	let filterName = document.getElementById("filter-drop");
-	let selectedFilter = filterName.options[filterName.selectedIndex].value;
-	let searchResult = invertedClass.searchIndex(searchValue, selectedFilter);
+  if (noFile) {
+    document.getElementById('alert-message')
+     .innerHTML = "No created index yet\n Create index first";
+    $('#alertError').modal('show');
+    return;
+  }
+  deleteTable();
+  let searchValue = $('#search-input').val();
+  let filterName = document.getElementById("filter-drop");
+  let selectedFilter = filterName.options[filterName.selectedIndex].value;
+  let searchResult = invertedClass.searchIndex(searchValue, selectedFilter);
 
-	if (selectedFilter === 'all') {
-		displayAllSearch(searchResult);
-	} else {
-		checkIndex(searchResult, invertedClass.jLength[selectedFilter],
-			selectedFilter);
-	}
+  if (selectedFilter === 'all') {
+    displayAllSearch(searchResult);
+  } else {
+    checkIndex(searchResult, invertedClass.jLength[selectedFilter],
+      selectedFilter);
+  }
 };
 
 
 // create index table upon selecting any option from select button
 $('#filter-drop').on('click', function () {
-	let filterName = $(this).val();
-	return filterName === null ? alert('No file to select') : searchFunction();
+  let filterName = $(this).val();
+  return filterName === null ? alert('No file to select') : searchFunction();
 });
 
 
 // Selected Json file's File Reader function
 let getFile = (filePath) => {
-	let regex = /^([a-zA-Z0-9\s_\\.\-:])+(.json)$/;
-	if (regex.test($("#fileUpload").val().toLowerCase())) {
-		let reader = new FileReader();
-		reader.onload = () => {
-			let jUpload = JSON.parse(reader.result);
-			if (invertedClass.isValidJson(reader.result)) {
-				if (!invertedClass.isJsonEmpty(jUpload) &&
-					invertedClass.validateJsonContent(jUpload)) {
-					invertedClass.json = jUpload;
-					invertedClass.fileName = filePath.name;
-				}
-				else {
-					document.getElementById('alert-message')
-						.innerHTML = "Invalid Json Content";
-					$('#alertError').modal('show');
-					setInputEmpty();
-					return;
-				}
-			} else {
-				document.getElementById('alert-message')
-					.innerHTML = "Invalid Json File";
-				$('#alertError').modal('show');
-			}
-		};
-		reader.readAsText(filePath);
-	} else {
-		document.getElementById('alert-message')
-			.innerHTML = "Upload a valid json file";
-		$('#alertError').modal('show');
-		setInputEmpty();
-		return;
-	}
+  let regex = /^([a-zA-Z0-9\s_\\.\-:])+(.json)$/;
+  if (regex.test($("#fileUpload").val().toLowerCase())) {
+    let reader = new FileReader();
+    reader.onload = () => {
+      let jUpload = JSON.parse(reader.result);
+      if (invertedClass.isValidJson(reader.result)) {
+        if (!invertedClass.isJsonEmpty(jUpload) &&
+          invertedClass.validateJsonContent(jUpload)) {
+          invertedClass.json = jUpload;
+          invertedClass.fileName = filePath.name;
+        }
+         else {
+          document.getElementById('alert-message')
+            .innerHTML = "Invalid Json Content";
+          $('#alertError').modal('show');
+          setInputEmpty();
+          return;
+        }
+      } else {
+        document.getElementById('alert-message')
+          .innerHTML = "Invalid Json File";
+        $('#alertError').modal('show');
+      }
+    };
+    reader.readAsText(filePath);
+  } else {
+    document.getElementById('alert-message')
+      .innerHTML = "Upload a valid json file";
+    $('#alertError').modal('show');
+    setInputEmpty();
+    return;
+  }
 };
 
 // To check if json file already exist
 let checkExistingFileName = () => {
-	invertedClass.names.forEach((name) => {
-		if (name === invertedClass.fileName) {
-			document.getElementById('alert-message').innerHTML = "File already exit";
-			$('#alertError').modal('show');
-			setInputEmpty();
-			throw true;
-		}
-	});
-	return false;
+  invertedClass.names.forEach((name) => {
+    if (name === invertedClass.fileName) {
+      document.getElementById('alert-message').innerHTML = "File already exit";
+      $('#alertError').modal('show');
+      setInputEmpty();
+      throw true;
+    }
+  });
+  return false;
 };
